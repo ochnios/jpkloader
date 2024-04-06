aea40f27546cfcfe92d77b7a1b3dbacbd0125883
Szymon Ochnio <szymonochnio15@gmail.com>
2024-04-06 17:53:42 +0200

    Initialize project

---
+++ src/main/java/pl/ochnios/jpkloader/JpkloaderApplication.java
@@ -0,0 +1,13 @@
+package pl.ochnios.jpkloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpkloaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpkloaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ochnios/jpkloader/JpkloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ochnios.jpkloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpkloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
