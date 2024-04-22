bc483cd17a8b9ae6eaf39e2e6628f785bfe29626
Szymon Ochnio <szymonochnio15@gmail.com>
2024-04-22 19:12:10 +0200

    Fix little bug in RachunekService

--- src/main/java/pl/ochnios/jpkloader/services/RachunekService.java
+++ src/main/java/pl/ochnios/jpkloader/services/RachunekService.java
@@ -19,23 +19,25 @@ public class RachunekService {
 
     @Transactional
     public ServiceResponse<Rachunek> createRachunek(String numer, String nip, String kodWaluty) {
+        var waluta = walutaRepository.findById(kodWaluty);
+        if (waluta.isEmpty()) {
+            return ServiceResponse.fail(String.format("Brak waluty o kodzie %s", kodWaluty));
+        }
+
         var current = rachunekRepository.findById(numer);
         if (current.isPresent()) {
             if (!current.get().getPodmiot().getNip().equals(nip)) {
                 return ServiceResponse.fail(String.format("Rachunek %s już istnieje dla NIPu %s", numer, nip));
             }
             if (!current.get().getWaluta().getKod().equals(kodWaluty)) {
-                return ServiceResponse.fail(String.format("Rachunek %s już istnieje z walutą %s", numer, kodWaluty));
+                return ServiceResponse.fail(String.format("Rachunek %s już istnieje z walutą %s",
+                        numer, current.get().getWaluta().getKod()));
             }
             return ServiceResponse.success(current.get());
         } else {
             var podmiot = podmiotWbRepository.findById(nip);
             if (podmiot.isEmpty()) {
                 return ServiceResponse.fail(String.format("Brak podmiotu o numerze NIP=%s", nip));
-            }
-            var waluta = walutaRepository.findById(kodWaluty);
-            if (waluta.isEmpty()) {
-                return ServiceResponse.fail(String.format("Brak waluty o kodzie ", kodWaluty));
             }
             var rachunek = rachunekRepository.save(new Rachunek(numer, podmiot.get(), waluta.get()));
             return ServiceResponse.success(rachunek);
